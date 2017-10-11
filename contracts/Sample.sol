1a2011280aa064aa6dd70ace90893f4cf1534a01
ktalanda <ktalanda@k2net.pl>
2017-10-11 22:01:22 +1100

    Update contract.

--- contracts/Sample.sol
+++ contracts/Sample.sol
@@ -3,12 +3,8 @@ pragma solidity ^0.4.8;
 contract Sample {
     address[16] public sampleArray;
 
-    function adopt(uint id) public returns (uint) {
+    function run(uint id) public returns (uint) {
         sampleArray[id] = msg.sender;
         return id;
-    }
-
-    function getAdopters() public returns (address[16]) {
-        return sampleArray;
     }
 }
--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
         development: {
             host: "localhost",
             port: 8545,
-            network_id: "1505302920641" // Match any network id
+            network_id: "*" // Match any network id
         }
     }
 };
