e28335be725cb71fa59ec87862929bfbac428c34
ktalanda <ktalanda@k2net.pl>
2017-10-21 18:58:25 +1100

    Use es6 spread.

--- src/app/AppReducer.js
+++ src/app/AppReducer.js
@@ -6,6 +6,7 @@ export default(state = initialState, action) => {
   switch (action.type) {
     case 'SET_COUNTER':
       return {
+        ...state,
         counter: action.newCounter
       }
     default:
